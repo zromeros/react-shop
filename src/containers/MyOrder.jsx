77ca6eddb764f5bd601e07e0467a64ea2dac107a
Zadkiel Romero <zadkielr@gmail.com>
2023-02-23 15:51:51 -0400

    connect context

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import OrderItem from "@components/OrderItem";
 import "@styles/MyOrder.scss";
 import arrow from "@icons/flechita.svg";
+import AppContext from "@context/AppContext";
 
 const MyOrder = () => {
+  const { state } = useContext(AppContext);
+
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
   return (
     <aside className="MyOrder">
       <div className="title-container">
@@ -11,12 +20,17 @@ const MyOrder = () => {
         <p className="title">My order</p>
       </div>
       <div className="my-order-content">
-        <OrderItem />
+        {state.cart.map((product) => {
+          return (
+            <OrderItem product={product} key={`orderItem-${product.id}`} />
+          );
+        })}
+
         <div className="order">
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -13,9 +13,17 @@ const useInitialState = () => {
       cart: [...state.cart, payload],
     });
   };
+
+  const removeFromCart = (payload) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((item) => item.id !== payload.id),
+    });
+  };
   return {
     state,
     addToCart,
+    removeFromCart,
   };
 };
 
